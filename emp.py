31476b83ee9ad88b86d8cd8c73b98202d9916e35
sufail <sufailps786@gmail.com>
2021-08-15 00:57:54 +0530

    erp using dictionary

--- emp.py
+++ emp.py
@@ -1,7 +1,7 @@
-print("employee management")
+print("employee management using dictionary")
 
-emp=[]
-
+#emp=[]
+empdict={}
 while True:
 
 	print("Enter 1 for add employee ")
@@ -12,42 +12,85 @@ while True:
 	print("enter 6 for quite")
 	choice=int(input())
 	if (choice == 1):
-		print("enter employee name :")
-		addname=input()
-		emp.append(addname)
+		addid=int(input("Enter Id\t"))
+		if addid not in empdict.keys():
+			addname=input("Enter name\t")
+			addage=int(input("Enter age\t"))
+			addgender=input("Gender\t\t")
+			addplace=input("Enter place\t")
+			addsalary=input("Enter salary\t")
+			addpcompany=input("Enter previous company\t")
+			addjdate=input("Enter joining data dd/mm/yyyy\t")
+			temp={
+				"name":addname,
+				"age":addage,
+				"gender":addgender,
+				"place":addplace,
+				"salary":addsalary,
+				"pcompany":addpcompany
+				}
+			empdict[addid] = temp
+		else:
+			print("ID is already in data")
+		
+#		emp.append(addname)
 	elif (choice == 2):
-		print(emp)
-		print("enter name for delete :")
-		dname=input()
-		emp.remove(dname)
-		print("deleted",emp)
+		print(empdict)
+		did=int(input("Enter Id\t"))
+		del empdict[did]
+		print("deleted",empdict)
 	elif (choice == 3):
-		print("enter name to search")
-		sname=input()
-		if sname in emp:
-			print(sname,"is found")
+		sname=input("Enter employee name\t")
+		for i in empdict.values():
+			if i["name"] == sname:
+				print(sname,"is found")
 		else:
 			print(sname, "not in data")
 	elif (choice == 4):
-		print("enter name for change")
-		cname=input()
-		i=emp.index(cname)
-		print("enter new name")
-		nname=input()
-		emp[i]=nname
-		print(emp)
+		cid=int(input("enter id of employee"))
+		if cid in empdict.keys():
+			ch=int(input("1 for change name \n 2 for age \n 3 for gender \n 4 for salary"))
+			if (ch == 1):
+				cname=input("enter new name")
+				empdict[cid]['name']=cname
+				print(empdict)
+
+			elif (ch == 2):
+				cage=input("Enter new age")
+				empdict[cid]['age']=cage
+				print(empdict)
+			elif (ch == 3):
+				cgender=input("Enter gender")
+				empdict[cid]['gender']=cgender
+				print(empdict)
+			elif (ch == 4):
+				csalary=input("Enter salary")
+				empdict[cid]['salary']=csalary
+				print(empdict)
+			else:
+				print("invalid entry")
+		else:
+			print("wrong employee id")
+		#cname=input()
+		#i=emp.index(cname)
+		#print("enter new name")
+		#nname=input()
+		#emp[i]=nname
+		#print(emp)
 	elif (choice == 5):
-		a=1
-		for j in emp:
+		print(empdict)
+		#a=1
+		#for j in emp:
 					
-			print(str(a) + "." + j)
-			a+=1
+	#		print(str(a) + "." + j)
+	#		a+=1
 		#	aaa=j
 			#ind=emp.index[aaa]
 		
 
 	elif (choice == 6):
 		break;
+
 	else:
 		print("invalid entry")
 
